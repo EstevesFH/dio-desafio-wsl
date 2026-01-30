23a81287bc0b03828c21af5440e45b999b114ad0
MarceloEstevesFH <marcelo@payhop.com.br>
2026-01-30 17:06:13 -0300

    Primeiro Commit

---
+++ src/components/ItemRepo/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { ItemContainer } from './styles.js'
+
+function ItemRepo({repo, handleRemoveRepo}) {
+
+  const handleRemove = (e) => {
+    e.preventDefault();
+    handleRemoveRepo(repo.id);
+  }
+
+  return (
+    <ItemContainer>
+      <h3>{repo.name}</h3>
+      <p>{repo.full_name}</p>
+      <a href={repo.html_url} rel="noreferrer" target="_blank">Ver repositório</a><br />
+      <a href="#" rel="noreferrer" className="remove" onClick={handleRemove}>Remover</a>
+      <hr />
+    </ItemContainer>
+  )
+}
+
+export default ItemRepo
